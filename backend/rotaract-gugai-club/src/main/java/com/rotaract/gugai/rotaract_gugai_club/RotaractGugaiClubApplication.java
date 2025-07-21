54e6e1ca6d0ec937b9543a507802c5b3bce0a494
Dhanush K <dhanushk8703@gmail.com>
2025-07-21 18:41:53 +0530

    all files and folder created

---
+++ backend/rotaract-gugai-club/src/main/java/com/rotaract/gugai/rotaract_gugai_club/RotaractGugaiClubApplication.java
@@ -0,0 +1,13 @@
+package com.rotaract.gugai.rotaract_gugai_club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaractGugaiClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotaractGugaiClubApplication.class, args);
+	}
+
+}
---
+++ backend/rotaract-gugai-club/src/test/java/com/rotaract/gugai/rotaract_gugai_club/RotaractGugaiClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rotaract.gugai.rotaract_gugai_club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotaractGugaiClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
